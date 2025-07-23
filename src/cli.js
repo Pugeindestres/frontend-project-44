481f6c21278fe6fbe335213584e1a675f02a5252
Pugeindestres <crazydevstuck@gmail.com>
2025-07-23 22:27:41 +0300

    add  new game fix 1.02

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,6 @@ import readlineSync from 'readline-sync'; //библиотека для чтен
 export function HelloUser() { //функция коотрая спрашивает у пользователя имя
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}`)
   console.log(`Welcom to brain-games, ${name}!`);
 }
