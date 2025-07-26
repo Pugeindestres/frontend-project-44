ea37a22f29e3f9bb7ec7c335bf8bb418028fd95e
Pugeindestres <crazydevstuck@gmail.com>
2025-07-26 08:42:01 +0300

    fix node 1.6

--- eslint.config.js
+++ eslint.config.js
@@ -4,31 +4,30 @@ import pluginJs from '@eslint/js'
 
 export default [
   
-  pluginJs.configs.recommended, // импорт рекомендуемых правил плагина js
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      ecmaVersion: 2021,
-      sourceType: 'module',
+    pluginJs.configs.recommended, // импорт рекомендуемых правил плагина js
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+        rules: {
+            semi: ['error', 'never'],
+            quotes: ['error', 'single'],
+            'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+            indent: ['error', 4],
+            'spaced-comment': ['error', 'always'],
+            'no-multiple-empty-lines': ['error', { max: 1 }],
+            'eol-last': ['error', 'always'],
+            'object-curly-spacing': ['error', 'always'],
+            'arrow-parens': ['error', 'as-needed'],
+            'quote-props': ['error', 'as-needed'],
+            'space-before-function-paren': ['error', 'never'],
+            'space-before-blocks': ['error', 'always'],
+        },
     },
-    rules: {
-      semi: ['error', 'never'], // запрет точек с запятой
-      quotes: ['error', 'single'], // одинарные кавычки
-      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
-      indent: ['error', 2],
-      'spaced-comment': ['error', 'always'],
-      'no-multiple-empty-lines': ['error', { max: 1 }],
-      'eol-last': ['error', 'always'],
-            
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-parens': ['error', 'as-needed'],
-      'quote-props': ['error', 'as-needed'],
-      'space-before-function-paren': ['error', 'never'],
-      'space-before-blocks': ['error', 'always'],
-    },
-  },
 ]
