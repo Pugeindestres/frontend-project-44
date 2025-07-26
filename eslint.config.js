581e33ecae4d354b8d405d71b34acd6111500c89
Pugeindestres <crazydevstuck@gmail.com>
2025-07-26 08:11:50 +0300

    fix node 1.1

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  pluginJs.configs.recommended, // вместо "plugin:js/recommended"
+  pluginJs.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
