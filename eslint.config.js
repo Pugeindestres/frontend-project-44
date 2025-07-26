b559668421ee47e6eaf17e45984ddb1ef3fc7317
Pugeindestres <crazydevstuck@gmail.com>
2025-07-26 07:26:14 +0300

    fix 1.1.11 node

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,24 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    env: {
+      node: true,
+      es2021: true,
+      browser: true,
+    },
+    extends: [
+      "airbnb",
+    ],
+    rules: {
+      // здесь ваши правила, например:
+      // "no-console": "off",
+    },
+  },
 ]);
