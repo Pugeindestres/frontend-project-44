906fc7145b2b6cdc521fe9153a626901e285ba89
Pugeindestres <crazydevstuck@gmail.com>
2025-07-26 08:30:11 +0300

    fix node 1.5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -13,30 +13,30 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".')
 const roundsCount = 3
 
 function isEven(number) {
-    return number % 2 === 0
+  return number % 2 === 0
 }
 
 for (let round = 1; round <= roundsCount; round += 1) {
-    const questionNumber = Math.floor(Math.random() * 100) + 1 // число от 1 до 100
-    console.log(`Question: ${questionNumber}`)
+  const questionNumber = Math.floor(Math.random() * 100) + 1 // число от 1 до 100
+  console.log(`Question: ${questionNumber}`)
 
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
+  const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
 
-    const correctAnswer = isEven(questionNumber) ? 'yes' : 'no'
+  const correctAnswer = isEven(questionNumber) ? 'yes' : 'no'
 
-    if (userAnswer !== 'yes' && userAnswer !== 'no') {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-        console.log(`Let's try again, ${userName}!`)
-        process.exit(1)
-    }
+  if (userAnswer !== 'yes' && userAnswer !== 'no') {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+    console.log(`Let's try again, ${userName}!`)
+    process.exit(1)
+  }
 
-    if (userAnswer === correctAnswer) {
-        console.log('Correct!')
-    } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-        console.log(`Let's try again, ${userName}!`)
-        process.exit(1)
-    }
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!')
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+    console.log(`Let's try again, ${userName}!`)
+    process.exit(1)
+  }
 }
 
 console.log(`Congratulations, ${userName}!`)
