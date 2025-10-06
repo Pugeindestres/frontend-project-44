b05cfa0d7bdcf82aa63e824f56093b3dd5358164
Pugeindestres <crazydevstuck@gmail.com>
2025-10-06 21:55:26 +0300

    new start

--- bin/brain-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import {HelloUser} from '../src/cli.js';
-
-HelloUser(); // вызов функции приветсвия и вопроса у пользователя
--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync'; // библиотека для чтения пользовательского ввода
-
-export function HelloUser() { // функция коотрая спрашивает у пользователя имя
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${name}`);
-    console.log(`Welcom to brain-games, ${name}!`);
-    return name;
-}
